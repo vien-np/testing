973bfc6271bf96c517473a6d84f5ee8dbedfe29d
vien khung <vien@rasia.info>
2016-03-09 17:04:21 +0700

    add

--- MQTT/app.js
+++ MQTT/app.js
@@ -65,7 +65,7 @@ Server.prototype.configureExpress = function() {
 };
 
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 //Start server at port 3100
 Server.prototype.startServer = function() {
